210b7ca75cca6432410e16293c75bf4d39b34c6e
Michael Zapf <m.zapf@mztx.de>
2024-12-23 09:22:19 +0100

    initial plugin skeleton

---
+++ wp-content/plugins/mztx-wp-simple-et-plugin/mztx-wp-simple-et-plugin.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * @wordpress-plugin
+ * Plugin Name:       mztx Simple Integration von evangelische-termine.de
+ * Plugin URI:        https://mztx.de
+ * Description:       Sehr einfache und nicht mächtige Möglichkeit, evangelische-termine.de einzubinden
+ * Version:           {{ version }}
+ * Author:            Michael Zapf
+ * Author URI:        https://mztx.de
+ * Update URI:        https://mztxde.github.io/mztx-wp-simple-et-plugin/manifest.json
+ * License:           MIT
+ * License URI:       https://raw.githubusercontent.com/michz/mztx-wp-simple-et-plugin/refs/heads/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace mztx\wp\plugin\SimpleEt;
+
+const MZTX_WP_SIMPLE_ET_PLUGIN_SLUG = 'mztx-wp-simple-et-plugin';
+const MZTX_WP_SIMPLE_ET_PLUGIN_VERSION = '{{ version }}';
+
+require_once __DIR__ . '/update.php';
+
+$mztxWpSimpleEtPluginUpdater = new Updater(
+    MZTX_WP_SIMPLE_ET_PLUGIN_SLUG,
+);
