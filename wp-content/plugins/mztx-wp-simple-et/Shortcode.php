7aae5b8478e47e8b3880fa5cfdeef9ae67533d2b
Michael Zapf <m.zapf@mztx.de>
2025-01-30 10:18:37 +0100

    iframe: disable cookies completly
    as it caused problems with chromium based browsers

--- wp-content/plugins/mztx-wp-simple-et/Shortcode.php
+++ wp-content/plugins/mztx-wp-simple-et/Shortcode.php
@@ -105,7 +105,7 @@ readonly class Shortcode
             );
 
         return '
-            <iframe class="evtermine-embedded iframe-height--' . $heightClass . '" src="' . htmlentities($url) . '"></iframe>
+            <iframe sandbox="allow-scripts" class="evtermine-embedded iframe-height--' . $heightClass . '" src="' . htmlentities($url) . '"></iframe>
         ';
     }
 }
